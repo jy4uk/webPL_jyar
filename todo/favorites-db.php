fbb788c27668fdf2e6c6a942bf789e8f4794e233
ar3wm <ar3wm@virginia.edu>
2020-04-09 14:15:54 -0400

    Modified SignUp page layout
    - Added an "Already have an account?" link/message to the SignUp page
    - Removed file path from the require('.../signInPage-db.php') in signUp and signInPage files

--- todo/favorites-db.php
+++ todo/favorites-db.php
@@ -1,22 +1,5 @@
 <?php 
 
-// require: if a required file is not found, reqire() produces a fatal error, the rest of the script won't run
-// include: if a required file is not found, include() thorws a warning, the rest of the script will run
-
-
-// Prepared statement (or parameterized statement) happens in 2 phases:
-//   1. prepare() sends a template to the server, the server analyzes the syntax
-//                and initialize the internal structure.
-//   2. bind value (if applicable) and execute
-//      bindValue() fills in the template (~fill in the blanks.
-//                For example, bindValue(':name', $name);
-//                the server will locate the missing part signified by a colon
-//                (in this example, :name) in the template
-//                and replaces it with the actual value from $name.
-//                Thus, be sure to match the name; a mismatch is ignored.
-//      execute() actually executes the SQL statement
-
-
 function addFavorite($username, $name, $link)
 {
 	global $db;
